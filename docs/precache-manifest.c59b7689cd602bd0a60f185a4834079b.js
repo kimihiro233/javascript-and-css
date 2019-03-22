c65ab3dfd9523cea7087b34a66b20637dd6904a3
kimihiro <kimihiro@live.cn>
2019-03-22 15:57:55 +0800

    add build file

---
+++ docs/precache-manifest.c59b7689cd602bd0a60f185a4834079b.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "735ab4f94fbcd57074377afca324c813",
+    "url": "/javascript-and-css/robots.txt"
+  },
+  {
+    "revision": "6aa4fd839c67037606dc",
+    "url": "/javascript-and-css/js/chunk-vendors.cff353a8.js"
+  },
+  {
+    "revision": "1e44023b951655165ff6",
+    "url": "/javascript-and-css/js/app.17fd7803.js"
+  },
+  {
+    "revision": "9b795269f291a8027253",
+    "url": "/javascript-and-css/js/about.08d8ac90.js"
+  },
+  {
+    "revision": "dffaeab3ecdd032eb367c7577f6b80ae",
+    "url": "/javascript-and-css/index.html"
+  },
+  {
+    "revision": "82b9c7a5a3f405032b1db71a25f67021",
+    "url": "/javascript-and-css/img/logo.82b9c7a5.png"
+  },
+  {
+    "revision": "1e44023b951655165ff6",
+    "url": "/javascript-and-css/css/app.f46505f1.css"
+  }
+];
