8d9c7a49457e9e43978f24947e2563c1019c96a9
kimihiro <kimihiro@live.cn>
2019-03-22 15:36:28 +0800

    init vue

---
+++ docs/precache-manifest.d5c3d16d3d46d3084e350b7ab29b76ba.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  },
+  {
+    "revision": "6aa4fd839c67037606dc",
+    "url": "/js/chunk-vendors.cff353a8.js"
+  },
+  {
+    "revision": "155ac016ed8502e68b1f",
+    "url": "/js/app.24d82b31.js"
+  },
+  {
+    "revision": "a5b2163bf37a90c76edd",
+    "url": "/js/about.7d44eef1.js"
+  },
+  {
+    "revision": "d18091c61895cf404738fe67fadaa421",
+    "url": "/index.html"
+  },
+  {
+    "revision": "82b9c7a5a3f405032b1db71a25f67021",
+    "url": "/img/logo.82b9c7a5.png"
+  },
+  {
+    "revision": "155ac016ed8502e68b1f",
+    "url": "/css/app.f46505f1.css"
+  }
+];
